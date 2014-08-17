7175d8d1f0427d3ea2d9721719762a8b5d00e275
fahmi <vambex.system@gmail.com>
2014-08-17 17:18:43 +0700

    add edit delete Kelas api

--- app/controllers/ApplicationKelas.java
+++ app/controllers/ApplicationKelas.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 import fahmi.lib.JsonHandler;
 import models.Kelas;
+import models.Siswa;
 import play.data.Form;
 import play.libs.Json;
 import play.mvc.Controller;
@@ -25,11 +26,30 @@ public class ApplicationKelas extends Controller{
 	}
 	
 	public static Result delete(){
-		return TODO;
+		Form<Kelas> frmKelas = Form.form(Kelas.class).bindFromRequest();
+    	String idKelas = frmKelas.data().get("idKelas");
+    	if(idKelas == null){
+    		return badRequest(JsonHandler.getSuitableResponse("require nim", false));
+    	}else {
+    		Kelas kelas = Kelas.finder.byId(Long.parseLong(idKelas));
+    		if(kelas == null){
+    			return badRequest(JsonHandler.getSuitableResponse("data not found", false));
+    		}
+    		kelas.delete();
+    		return ok(JsonHandler.getSuitableResponse("success delete data", true));
+    	}
 	}
 	
 	public static Result edit(){
-		return TODO;
+		Form<Kelas> frmKelas = Form.form(Kelas.class).bindFromRequest();
+		if(frmKelas.hasErrors()){
+			return badRequest(JsonHandler.getSuitableResponse(JsonHandler.getErrorMessage(frmKelas), false));
+		}
+		else {
+			Kelas kelas = frmKelas.get();
+			kelas.update();
+			return ok(JsonHandler.getSuitableResponse(Json.toJson(kelas), true));
+		}
 	}
 	
 	public static Result list(){
