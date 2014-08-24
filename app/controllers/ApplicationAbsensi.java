7a50125888e7817bbaec20e9eb142fb997148c3b
fahmi <vambex.system@gmail.com>
2014-08-24 17:20:25 +0700

    add absensi class

---
+++ app/controllers/ApplicationAbsensi.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class ApplicationAbsensi extends Controller{
+	
+	public static Result absenSiswa(){
+		return TODO;
+	}
+
+}
